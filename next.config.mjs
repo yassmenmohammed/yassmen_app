8dc0ae9f59a2dd6023931c49c13bd6e1679e9fb3
MahmoudEmad <mahmoud3m6d@gmail.com>
2026-04-26 00:59:43 +0300

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
-};
-module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
+};
+
 export default nextConfig;
